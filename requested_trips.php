3eb2e5d093c3b7021a3470fbcb6e2330df50d512
Utkarsh <utkarsh.chauhan@iitrpr.ac.in>
2017-07-18 01:25:28 +0530

    Add basic backend for displaying requested trips

---
+++ requested_trips.php
@@ -0,0 +1,144 @@
+<?php
+	session_start();
+if(!isset($_SESSION['id'])){
+	header("Location:index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Dashboard</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery-2.1.3.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <style type="text/css">
+	.container{
+		text-align: center;
+	}
+
+	.red{
+    color: red;
+}
+.borderround {
+	border:1px solid #DEDEDE;
+	border-radius: 10px;
+
+}
+.righticon {
+	float: right;
+	
+}
+.smallfont {
+	font-size: .8em;
+}
+.navbar{
+	position: relative;
+}
+.logoimg {
+			float: left;
+			margin-right: 20px; 
+			margin-top: -5px;
+		}
+.clearicon {
+	float:right;
+	margin-right: 20px;
+	
+}	
+.borderbottom {
+margin: 0;
+}
+  </style>
+</head>
+
+<body>
+	<div class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header pull-left">
+			<div class="navbar-brand"><a href="user.php" class="linknone">Carpooling</a><a href="http://www.iitrpr.ac.in/" target="blank"><img class="logoimg" src="img/iitrpr.png" width="30px" height="30px"></a></div>
+			
+		</div>
+		<div class="pull-right">
+			<ul class="navbar-nav nav">
+			<li><a href="edit_profile.php">Edit Profile</a></li>
+			<li><a href="logout.php">Log Out</a></li>
+			</ul>
+			
+		</div>
+	</div>
+	</div>
+
+<?php 
+	include("connection.php");
+?>
+<div class="container">
+	<?php
+
+		echo '<h2>Hi, '.$_SESSION['name'].'! Welcome to Carpooling !!</h2>';
+
+	?>
+
+</div>
+
+<?php 
+
+	// Get all the trips between source and destination
+	$query="SELECT * FROM request";
+
+	echo "<br>";
+	
+	// Execute the query and store the result
+	$result=mysqli_query($link,$query);
+
+	echo '<div class="col-md-2 col-md-offset-8">';
+	$table = '<table class="table table-striped">
+	<thead>
+		<tr>
+		<th style="text-align:center">User</th>
+		<th style="text-align:center">Rating</th>
+		<th style="text-align:center">Source</th>
+		<th style="text-align:center">Destination</th>
+		<th style="text-align:center">Date and Time</th>
+		<th style="text-align:center">Number of Passengers</th>';
+	$table.= '
+		</tr>
+	</thead>
+	<tbody>';
+
+	while($row = mysqli_fetch_array($result))
+	{
+
+		$query1="SELECT * FROM user WHERE user_id='".mysqli_real_escape_string($link,$row['requestuser_id'])."'";
+		$result1=mysqli_query($link,$query1);
+		$row1=mysqli_fetch_array($result1);
+		$passengers = $row['passengers'];
+	 	$source = $row['source'];
+	 	$destination = $row['destination'];
+	 	$date_time = $row['date_time'];
+	 	$user = $row1['name'];
+	 	$rating = $row1['rating'];
+
+		$table.= '<tr><td style="text-align:center;">'.$user.'</td>';
+		$table.= '<td style="text-align:center;">'.$rating.'</td>';
+		$table.= '<td style="text-align:center;">'.$source.'</td>';
+		$table.= '<td style="text-align:center;">'.$destination.'</td>';
+		$table.= '<td style="text-align:center;">'.$date_time.'</td>';
+		$table.= '<td style="text-align:center;">'.$passengers.'</td>';
+	 	$table.= '</tr>';
+	 }
+
+	    $table.= '
+	    </tbody>
+	  </table>';
+		echo '</div>';
+		echo $table;
+
+
+?>
+
+</body>
+</html>
+
+
